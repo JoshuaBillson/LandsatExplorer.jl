af5ada478f5a10a75c6f4aeae2850f915e2e0159
JoshuaBillson <jmbillson@outlook.com>
2024-02-17 04:08:11 -0700

    Documentation

--- src/api.jl
+++ src/api.jl
@@ -4,7 +4,7 @@
 Search for Landsat scenes belonging to the given satellite and processing level.
 
 # Parameters
-- `satellite`: One of "LANDSAT_5", "LANDSAT_7", or "LANDSAT_8", or "LANDSAT_9".
+- `satellite`: One of `"LANDSAT_5"`, `"LANDSAT_7"`, or "`LANDSAT_8"`, or "`LANDSAT_9"`.
 - `level`: The processing level; either 1 or 2.
 
 # Keywords
@@ -90,7 +90,7 @@ Lookup the entity ID for the given Landsat scene.
 
 # Example
 ```julia
-julia> LE.get_entity_id("LC08_L2SP_043024_20200802_20200914_02_T1")
+julia> get_entity_id("LC08_L2SP_043024_20200802_20200914_02_T1")
 "LC80430242020215LGN00"
 ```
 """
